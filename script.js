74c0708763c5e7bb38c6de67feabdc45bd71966d
Matheus Quelucci <58993586+mquelucci@users.noreply.github.com>
2022-07-06 20:28:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const music = new Audio('musica.mp3');
+
+function musicadefundo(){
+    document.getElementById("audio").play();
+}
+
+function login(){
+    alert("Login efetuado com sucesso!");
+}
+
+function impressao(){
+    alert("Impressão iniciada!");
+}
+
+function off(){
+    alert("ATENÇÃO!!! Você está offline!")
+}
+
